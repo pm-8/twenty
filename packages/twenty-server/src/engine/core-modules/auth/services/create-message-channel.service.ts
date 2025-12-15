09174edeb09974f91af76320fad887130f078bce
neo773 <neo773@protonmail.com>
2025-12-15 20:23:19 +0530

    fix lint

--- packages/twenty-server/src/engine/core-modules/auth/services/create-message-channel.service.ts
+++ packages/twenty-server/src/engine/core-modules/auth/services/create-message-channel.service.ts
@@ -6,7 +6,6 @@ import { v4 } from 'uuid';
 import { type WorkspaceEntityManager } from 'src/engine/twenty-orm/entity-manager/workspace-entity-manager';
 import { GlobalWorkspaceOrmManager } from 'src/engine/twenty-orm/global-workspace-datasource/global-workspace-orm.manager';
 import { buildSystemAuthContext } from 'src/engine/twenty-orm/utils/build-system-auth-context.util';
-import { type ConnectedAccountWorkspaceEntity } from 'src/modules/connected-account/standard-objects/connected-account.workspace-entity';
 import {
   MessageChannelPendingGroupEmailsAction,
   MessageChannelSyncStage,
