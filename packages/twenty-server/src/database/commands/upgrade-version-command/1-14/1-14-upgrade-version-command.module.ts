431017acccbcb2482fa875c4c98d1f242dcc7b50
martmull <martmull@hotmail.fr>
2025-12-15 19:26:19 +0100

    Update migration command

--- packages/twenty-server/src/database/commands/upgrade-version-command/1-14/1-14-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-14/1-14-upgrade-version-command.module.ts
@@ -5,10 +5,11 @@ import { UpdateCreatedByEnumCommand } from 'src/database/commands/upgrade-versio
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
 import { WorkspaceSchemaManagerModule } from 'src/engine/twenty-orm/workspace-schema-manager/workspace-schema-manager.module';
+import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([WorkspaceEntity]),
+    TypeOrmModule.forFeature([WorkspaceEntity, FieldMetadataEntity]),
     DataSourceModule,
     WorkspaceSchemaManagerModule,
   ],
